e40e5d20639156195c7d2980666d54dcf695e6c9
Manta Gel <ida.jhn@gmail.com>
2021-05-11 23:22:35 +0200

    tried to add highlight of long sentences, didn't work

--- longSentenceCounter.js
+++ longSentenceCounter.js
@@ -14,23 +14,36 @@ function count() {
     function longSentenceCounter() {
         let numberOfLongSentences = 0;
         let longSentencesSpec = "";
+        let allSentences = "";
         //for every sentence, create an array of words (separated by " ")
         for (let sentence = 0; sentence < arrayOfSentences.length; sentence++) {
             arrayOfWords = arrayOfSentences[sentence].split(" ");
             // JavaScript adds an empty index at the end of the array. Removing.
             arrayOfWordsLen = arrayOfWords.length -1
+            // add each sentence back to the text
+            
             // count the words in each sentence and log the contents of any sentence that is longer than 26 words
-            if (arrayOfWordsLen > 26) {
-                //console.log('This sentence is ' + arrayOfWordsLen + ' words long: ' + arrayOfSentences[sentence])
-                //document.getElementById("longSentences").innerHTML = 'This sentence is ' + arrayOfWordsLen + ' words long: ' + arrayOfSentences[sentence];
+            if (arrayOfWordsLen <= 26) {
+                // add unchanged into new text block 
+                allSentences += arrayOfSentences[sentence]; 
+            } else if (arrayOfWordsLen > 26) {
                 // add to total of long sentences
                 numberOfLongSentences += 1;
+                // add information about specific sentences
                 longSentencesSpec += 'This sentence is ' + arrayOfWordsLen + ' words long: ' + arrayOfSentences[sentence] + '. \n';
+                // convert index into string to be able to apply style
+                // let longSentenceString = arrayOfSentences[sentence].toString();
+                // add with highlight into new text block 
+                // NOT WORKING allSentences += longSentenceString.style.color="red";
             }
+
         }
+    
         //console.log('The text contains ' + longSentences + ' sentence(s) with more than 26 words.')
-        document.getElementById("numberOfLongSentences").value = 'The text contains ' + numberOfLongSentences + ' sentence(s) with more than 26 words.';
-        document.getElementById("longSentencesSpec").value = longSentencesSpec;
+        document.getElementById("numberOfLongSentences").innerHTML = 'The text contains ' + numberOfLongSentences + ' sentence(s) with more than 26 words.';
+        document.getElementById("longSentencesSpec").innerHTML = longSentencesSpec;
+        document.getElementById("allSentences").innerHTML = allSentences;
     }
+
     longSentenceCounter();
 }
