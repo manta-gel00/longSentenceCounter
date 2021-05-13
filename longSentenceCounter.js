d82f35f360432ea9bab5e9c80cd669075a0490df
manta-gel00 <77808566+manta-gel00@users.noreply.github.com>
2021-05-13 14:58:39 +0200

    Update longSentenceCounter.js

--- longSentenceCounter.js
+++ longSentenceCounter.js
@@ -1,13 +1,14 @@
 function count() { 
     let text = document.getElementById("rawText").value;
     //number of words that defines a long sentence
-    let breakPoint = 26
+    let breakPoint = document.getElementById("breakPoint").value;
 
     //let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est lorem, facilisis eu ipsum vel, semper hendrerit dolor. Quisque malesuada viverra viverra. Sed aliquet erat urna, et eleifend massa elementum at. Proin mollis enim sed elementum malesuada. Curabitur et congue lectus, et ultricies neque. Pellentesque accumsan sapien at imperdiet viverra. Donec faucibus sem massa, vel vehicula libero commodo ac. Proin porta arcu in tellus molestie, at venenatis ante pretium. Aenean congue ligula ac massa mattis, sit amet tincidunt leo imperdiet. Pellentesque consectetur eros id dolor hendrerit, sed scelerisque lectus ullamcorper. Pellentesque dictum scelerisque nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus tempor luctus elementum. Aenean facilisis ullamcorper maximus. Duis pellentesque at dui vel fringilla."
     
     // create array of sentences, separated by "."
     arrayOfSentences = text.split(".");
 
+    // count the number of long sentences in the input text
     function longSentenceCounter() {
         let numberOfLongSentences = 0;
         let longSentencesSpec = "";
@@ -37,5 +38,8 @@ function count() {
             document.getElementById("longSentencesSpec").innerHTML = longSentencesSpec;
         }
     }
+    //clear list of found sentences if the user runs the counter multiple times
+    document.getElementById("longSentencesSpec").innerHTML = ""
+
     longSentenceCounter();
 }
