232000bf4e73bab981f1fd597ea001a94b0884ec
Manta Gel <ida.jhn@gmail.com>
2021-05-13 11:46:11 +0200

    Converted strings to template literals. Turned breakpoint into variable.

--- longSentenceCounter.js
+++ longSentenceCounter.js
@@ -1,7 +1,10 @@
 function count() { 
- let text = document.getElementById("rawText").value;
+    let text = document.getElementById("rawText").value;
+    //number of words that defines a long sentence
+    let breakPoint = 26
 
     //let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est lorem, facilisis eu ipsum vel, semper hendrerit dolor. Quisque malesuada viverra viverra. Sed aliquet erat urna, et eleifend massa elementum at. Proin mollis enim sed elementum malesuada. Curabitur et congue lectus, et ultricies neque. Pellentesque accumsan sapien at imperdiet viverra. Donec faucibus sem massa, vel vehicula libero commodo ac. Proin porta arcu in tellus molestie, at venenatis ante pretium. Aenean congue ligula ac massa mattis, sit amet tincidunt leo imperdiet. Pellentesque consectetur eros id dolor hendrerit, sed scelerisque lectus ullamcorper. Pellentesque dictum scelerisque nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus tempor luctus elementum. Aenean facilisis ullamcorper maximus. Duis pellentesque at dui vel fringilla."
+    
     // create array of sentences, separated by "."
     arrayOfSentences = text.split(".");
 
@@ -11,18 +14,28 @@ function count() {
         // for every sentence, create an array of words separated by " "
         for (let sentence = 0; sentence < arrayOfSentences.length; sentence++) {
             arrayOfWords = arrayOfSentences[sentence].split(" ");
-            // counting number of words in each sentence. JavaScript adds an empty index at the end of the array, removing.
+            // count number of words in each sentence. JavaScript adds an empty index at the end of the array, removing.
             arrayOfWordsLen = arrayOfWords.length -1
-            // count the words in each sentence and log the contents of any sentence that is longer than 26 words
-            if (arrayOfWordsLen > 26) {
-                // add to total of long sentences
+            // count words in each sentence and add to total number of long sentences
+            if (arrayOfWordsLen > breakPoint) {
                 numberOfLongSentences += 1;
-                longSentencesSpec += 'This sentence is ' + arrayOfWordsLen + ' words long: ' + arrayOfSentences[sentence] + '. \n';
+                longSentencesSpec += `<strong>This sentence is ${arrayOfWordsLen} words long:</strong> ${arrayOfSentences[sentence]} <br><br>`;
             }
         }
-        //console.log('The text contains ' + longSentences + ' sentence(s) with more than 26 words.')
-        document.getElementById("numberOfLongSentences").innerHTML = 'The text contains ' + numberOfLongSentences + ' sentence(s) with more than 26 words.';
-        document.getElementById("longSentencesSpec").innerHTML = longSentencesSpec;
+        
+        // output messages to HTML and console with results
+        if (numberOfLongSentences === 0) {
+            console.log(`The counter found 0 long sentences.`)
+            document.getElementById("numberOfLongSentences").innerHTML = `0 sentences are longer than ${breakPoint} words.`;
+        } else if (numberOfLongSentences === 1) {
+            console.log(`The counter found 1 long sentence.`)
+            document.getElementById("numberOfLongSentences").innerHTML = `1 sentence is longer than ${breakPoint} words.`;
+            document.getElementById("longSentencesSpec").innerHTML = longSentencesSpec;
+        } else if (numberOfLongSentences > 1) {
+            console.log(`The counter found ${numberOfLongSentences} long sentences.`)
+            document.getElementById("numberOfLongSentences").innerHTML = `${numberOfLongSentences} sentences are longer than ${breakPoint} words.`;
+            document.getElementById("longSentencesSpec").innerHTML = longSentencesSpec;
+        }
     }
     longSentenceCounter();
 }
